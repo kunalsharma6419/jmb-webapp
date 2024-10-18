cf7d0613c9f4ff33fd4097a1f72fd2cc328a55bf
themehakcodes <codewithmehak@gmail.com>
2024-10-18 16:50:23 +0530

    Fixes

--- database/migrations/2024_10_16_123052_add_is_superadmin_to_users_table.php
+++ database/migrations/2024_10_16_123052_add_is_superadmin_to_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
                   ->default(0) // Default is not a superadmin
                   ->nullable()
                   ->after('profile_status'); // Place it after 'profile_status'
-                  $table->boolean('is_admin')->default(0)->nullable();
+                  $table->boolean('is_admin')->default(0)->nullable()->after('is_superadmin');
         });
     }
 
@@ -29,7 +29,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             // Dropping the 'is_superadmin' column during rollback
             $table->dropColumn('is_superadmin');
-            $table->dropColumn('is_admin');
+           
         });
     }
 };
